008554f6729a99ddf2f4fb20b235e6aad6985173
Mario <modulor@hotmail.es>
2020-11-10 18:02:40 -0600

    create/add components: home, products, contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+const routes: Routes = [
+  {
+    path: '/home',
+    component: HomeComponent
+  },
+  {
+    path: '/products',
+    component: ProductsComponent
+  },
+  {
+    path: '/contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
